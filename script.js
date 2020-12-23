79bf244eea0440fa525892ef7e9c906b5dd6e69b
William Touboul <Touboul.william@gmail.com>
2020-12-23 16:24:30 +0100

    Rendu ex1p2

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeImage() {
+    document.getElementById("image1").src="images/image1_2.jpg";
+}
+function changeImage2() {
+    document.getElementById("image1").src="images/image1.jpg";
+}
